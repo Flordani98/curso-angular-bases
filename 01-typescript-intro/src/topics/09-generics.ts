c3eae9d03c14e57575acd9d05757605d9b424269
Florencia <flordani98@gmail.com>
2023-10-18 14:51:54 -0300

    genericos

--- 01-typescript-intro/src/topics/09-generics.ts
+++ 01-typescript-intro/src/topics/09-generics.ts
@@ -1,8 +1,15 @@
 
 //tratar de no utilizar any ya q cuando se utiliza se le quita todas las restricciones que TypeScript tiene 
+//en su lugar podemos utilizar genericos
 
-export function whatsMyType(argument: any):any{
-    return argument
+export function whatsMyType<T>(argument: T): T{
+    return argument;
 }
 
-const amIString = whatsMyType('Hola mundo');
+let amIString = whatsMyType<string>('Hola mundo'); //le digo a la funcion que el parametro es especificamente un string, sino coloco en el parametro el string me marca error
+let amINumber = whatsMyType<number>(100);
+let amIArray = whatsMyType<number[]>([1,2,3,4,5]);
+
+console.log(amIString);
+console.log(amINumber);
+console.log(amIArray);
