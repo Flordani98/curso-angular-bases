97c26156aac0a93e03d3f18c14341aaf497c0488
Florencia <flordani98@gmail.com>
2023-10-23 14:26:22 -0300

    gifsService
    validacion de gifsService
    
    decorador viewChild

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs-service.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-box.component.css']
 })
 export class SearchBoxComponent {
+//viewchild: nos sirve para poder tomar una referencia local
+  @ViewChild('txtTagInput') //pide 1 o 2 argumentos, el cual el argumento es el selector(o funcion, etc) el nombre del html que quiero tomar, 
+  public tagInput!: ElementRef<HTMLInputElement> //especificamos el tipo de elemento que hacemos referencia
+
+  constructor(private gifsService: GifsService){ //injectamos el servicio
+  
+  
+  }
+  // searchTag(newTag: string){
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+  }
 
 }
